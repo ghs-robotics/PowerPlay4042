015b3636c35ebf8c1d7a02360ed98824e6e0c977
Ivan <ivanxu1116@gmail.com>
2022-09-12 16:29:25 -0700

    framework (bare minimum)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele1.java
@@ -1,4 +1,68 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-public class Tele1 {
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp
+//Want to try using Opmode instead of LinearOp since I heard this is better for TeleOp
+public class Tele1 extends LinearOpMode {
+
+    BNO055IMU imu;
+    Orientation angles;
+    ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+        while (opModeIsActive()){
+            //reset lift at start
+            double sec = runtime.seconds();
+            boolean release = sec < 3;
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////           Controller 1           ////////////////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //gyro angle setting
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+            //driving
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////           Controller 2           ////////////////////////////////
+            //////////////////////////////////////////////////////////////////////////////////////////////////
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////           Telemetry           /////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////
+
+            telemetry.addData("gamepad2.right_stick_y", gamepad2.right_stick_y);
+            telemetry.addData("gamepad2.right_stick_x", gamepad2.right_stick_x);
+            telemetry.addData("gamepad2.left_stick_y", gamepad2.left_stick_y);
+            telemetry.addData("gamepad2.a", gamepad2.a);
+            telemetry.addData("gamepad2.y", gamepad2.y);
+            /*telemetry.addData("z axis", angles.firstAngle);
+            telemetry.addData("y axis", angles.secondAngle);
+            telemetry.addData("x axis", angles.thirdAngle);
+            telemetry.addData("shooter power variable", shooterPower); */
+            telemetry.update();
+        }
+    }
 }
