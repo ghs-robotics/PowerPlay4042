8363362b2a2ab9a4bfbc849393d384607c20f93c
Ivan <ivanxu1116@gmail.com>
2022-09-12 16:29:25 -0700

    framework (bare minimum)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/DriveBase.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class DriveBase {
+    public double speed = 1.0;
+
+    public DcMotor leftFrontDrive;
+    public DcMotor rightFrontDrive;
+    public DcMotor leftRearDrive;
+    public DcMotor rightRearDrive;
+
+    public Telemetry telemetry;
+    public HardwareMap hardwareMap;
+    BNO055IMU imu;
+    Orientation angles;
+    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+    public DriveBase (HardwareMap hardwareMap, Telemetry telemetry){
+        this.hardwareMap = hardwareMap;
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        leftRearDrive = hardwareMap.get(DcMotor.class, "leftRearDrive");
+        rightRearDrive = hardwareMap.get(DcMotor.class, "rightRearDrive");
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        this.telemetry = telemetry;
+    }
+
+    public double getGyroAngle() {
+        return angles.firstAngle;
+    }
+
+    public void calculateDrivePower(double x, double y, double r){
+        r = -r;
+        double lf = r - x + y;
+        double lr = r + x + y;
+        double rf = r - x - y;
+        double rr = r + x - y;
+
+        sendDrivePower(lf, lr, rf, rr);
+    }
+
+    public void sendDrivePower(double lf, double lr, double rf, double rr){ //reversed to match motor polarity or something
+        leftFrontDrive.setPower(-1 * lf);
+        leftRearDrive.setPower(-1 * lr);
+        rightFrontDrive.setPower(rf);
+        rightRearDrive.setPower(rr);
+    }
+
+    public void driveForward(double speed) {
+        this.speed = speed;
+        leftFrontDrive.setPower(speed);
+        rightFrontDrive.setPower(speed);
+        leftRearDrive.setPower(speed);
+        rightRearDrive.setPower(speed);
+    }
+    public void driveRight(double speed) {
+        this.speed = speed;
+        leftFrontDrive.setPower(speed);
+        rightFrontDrive.setPower(-speed);
+        leftRearDrive.setPower(-speed);
+        rightRearDrive.setPower(speed);
+    }
+    public void turnRight(double speed) {
+        this.speed = speed;
+        leftFrontDrive.setPower(speed);
+        rightFrontDrive.setPower(-speed);
+        leftRearDrive.setPower(speed);
+        rightRearDrive.setPower(-speed);
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Robot {
+
+    public DcMotor intakeMotor;
+    public DcMotor shooterMotor;
+    public DcMotor extendingLiftMotor;
+
+    public Servo indexingServo;
+    public Servo liftingServo;
+
+    //we should rename these
+
 }
