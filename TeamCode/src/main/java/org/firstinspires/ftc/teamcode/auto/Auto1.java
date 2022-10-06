4bcab14029bc0ad5c256b27fbabb7e6b176883f0
wtheathOTG <williamtheath@icloud.com>
2022-10-06 16:56:22 -0700

    Added base of Autonomous State Machine.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Auto1.java
@@ -2,13 +2,14 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.robot.*;
+import org.firstinspires.ftc.teamcode.robot.Robot;
 @Autonomous(name = "Auto1")
 public class Auto1 extends LinearOpMode {
 
+    private int _mainState = 0; // 0 = ReadSleeve | 1 = ConeActions | 2 = Parking
+    private int _coneAction = 0; // 0 = place cone at pos #1
+    private int _currentStep = 0; // current step in coneAction
 
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
@@ -21,8 +22,34 @@ public class Auto1 extends LinearOpMode {
         if (opModeIsActive()) {
             // Put run blocks here.
             while (opModeIsActive()) {
-
+                MainState();
             }
+        }
+    }
+    private void MainState() {
+        switch( _mainState ) {
+            case 0:
+
+                break;
+
+            case 1:
+                ConeActions();
+                break;
+
+            case 2:
+
+                break;
+        }
+    }
+    private void ConeActions() {
+        switch ( _coneAction ) {
+            case 0:
+                switch ( _coneAction ) {
+                    case 0:
+
+                        break;
+                }
+                break;
         }
     }
 }
