48bdc777a1f361117c030a24de62ecde880b11d0
ManyiZ <zhaomanyi2005@gmail.com>
2022-10-20 21:04:49 -0700

    getting rid of useless code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/drive/opmode/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/drive/opmode/LocalizationTest.java
@@ -30,7 +30,7 @@ public class LocalizationTest extends LinearOpMode {
                             -gamepad1.left_stick_y,
                             -gamepad1.left_stick_x,
                             -gamepad1.right_stick_x
-                    )
+                            )
             );
 
             drive.update();
