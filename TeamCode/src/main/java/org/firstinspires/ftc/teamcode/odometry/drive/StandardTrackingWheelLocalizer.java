4460615e73878e0decabd5ceec50daeb2d5673f4
wtheathOTG <williamtheath@icloud.com>
2022-10-27 15:45:11 -0700

    Added odometry folder to master from odometry2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/drive/StandardTrackingWheelLocalizer.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.odometry.drive;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.odometry.util.Encoder;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Sample tracking wheel localizer implementation assuming the standard configuration:
+ *
+ *    /--------------\
+ *    |     ____     |
+ *    |     ----     |
+ *    | ||        || |
+ *    | ||        || |
+ *    |              |
+ *    |              |
+ *    \--------------/
+ *
+ */
+@Config
+public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
+    public static double TICKS_PER_REV = 8192;
+    public static double WHEEL_RADIUS = 17.5 / 25.4; // in
+    public static double GEAR_RATIO = 1.0 / 19.2; // output (wheel) speed / input (encoder) speed
+
+    public static double LATERAL_DISTANCE = 10.25; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -46.25 / 25.4; // in; offset of the lateral wheel
+
+    public static double X_MULTIPLIER = 21.55729023;
+    public static double Y_MULTIPLIER = 1;
+
+    private Encoder leftEncoder, rightEncoder, frontEncoder;
+
+    public StandardTrackingWheelLocalizer(HardwareMap hardwareMap) {
+        super(Arrays.asList(
+                new Pose2d(0, LATERAL_DISTANCE / 2, 0), // left
+                new Pose2d(0, -LATERAL_DISTANCE / 2, 0), // right
+                new Pose2d(FORWARD_OFFSET, 0, Math.toRadians(90)) // front
+        ));
+
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "LBDrive"));
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RBDrive"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "LFDrive"));
+
+        //TODO need to change it depending on new drivebase
+        leftEncoder.setDirection(Encoder.Direction.FORWARD);
+        rightEncoder.setDirection(Encoder.Direction.REVERSE);
+    }
+
+    public static double encoderTicksToInches(double ticks) {
+        return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+    }
+
+    @NonNull
+    @Override
+    public List<Double> getWheelPositions() {
+        return Arrays.asList(
+                encoderTicksToInches(leftEncoder.getCurrentPosition()) * X_MULTIPLIER,
+                encoderTicksToInches(rightEncoder.getCurrentPosition()) * X_MULTIPLIER,
+                encoderTicksToInches(frontEncoder.getCurrentPosition()) * Y_MULTIPLIER
+        );
+    }
+
+    @NonNull
+    @Override
+    public List<Double> getWheelVelocities() {
+        // TODO: If your encoder velocity can exceed 32767 counts / second (such as the REV Through Bore and other
+        //  competing magnetic encoders), change Encoder.getRawVelocity() to Encoder.getCorrectedVelocity() to enable a
+        //  compensation method
+
+        return Arrays.asList(
+                encoderTicksToInches(leftEncoder.getCorrectedVelocity()) * X_MULTIPLIER,
+                encoderTicksToInches(rightEncoder.getCorrectedVelocity()) * X_MULTIPLIER,
+                encoderTicksToInches(frontEncoder.getCorrectedVelocity()) * Y_MULTIPLIER
+        );
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/trajectorysequence/EmptySequenceException.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.odometry.trajectorysequence;
+
+
+public class EmptySequenceException extends RuntimeException { }
