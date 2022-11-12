43c4b9e54bcb515c3b3e68e7e7af9a062d891baa
wtheathOTG <williamtheath@icloud.com>
2022-11-11 17:53:54 -0800

    step forcing getPoleHeight funtion to appear part 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Arm.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
